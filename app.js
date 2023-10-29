9d3c5234298bc275d622086563edceeb0043e358
Rakshit Rajput <69393051+Rakshit-rajput@users.noreply.github.com>
2023-10-29 09:58:53 +0000

    commit

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+//connectDB
+const connectDB =require('./db/connect')
+//routers
+const authRouter = require('./routes/auth')
+const jobRouter = require('./routes/jobs')
+
 //error Handler
 const NotFoundMiddleware = require('./middlewares/not-found')
 const errorHandlerMiddleware = require('./middlewares/error-handler')
@@ -10,6 +16,13 @@ const errorHandlerMiddleware = require('./middlewares/error-handler')
 app.use(express.json());
 
 //routes
+app.use('/api/v1/auth',authRouter)
+app.use('/api/v1/jobs',jobRouter)
+
+app.use(NotFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
 app.get('/',(req,res)=>{
 res.send('jobs api');
 });
@@ -21,6 +34,7 @@ const port = process.env.PORT || 3000;
 
 const start = async()=>{
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port,()=>
         console.log(`Server is listening on port${port}...`)
         
--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-}
-
-module.exports = connectDB
+  return mongoose
+    .connect(url)
+    .then(() => console.log("Connected to the DB..."))
+    .catch((err) => console.log(err));
+};
+module.exports = connectDB;
